05e663ea7436ff487e300918a61f0ae8a82796d1
Binder Martin Mátyás <bindmart@bottyan.openedu.hu>
2024-05-28 12:47:01 +0200

    fixes

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -6,19 +6,20 @@ use App\Models\Work;
 use Carbon\Carbon;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 use Illuminate\View\View;
 
 class JobController extends Controller
 {
     public function index(): View {
-        $works = Work::where('created_at', '>=', Carbon::now()->startOfMonth())->with('user')->get();
+        $works = Work::where('created_at', '>=', Carbon::now()->startOfMonth())->with('user')->orderByDesc('created_at')->get();
         return view('jobs', ['works' => $works]);
     }
 
     public function finish(Work $id): RedirectResponse {
         $id->is_finished = true;
         $id->save();
-        return redirect('works.index');
+        return Redirect::route('works.index');
     }
 
     public function create(Request $request): RedirectResponse {
@@ -28,6 +29,6 @@ class JobController extends Controller
             'tax_percentage' => 'numeric|min:0|max:100',
         ]);
         Work::create(array_merge($data, ['user_id' => auth()->user()->id, 'is_finished' => false]));
-        return redirect('works.index');
+        return Redirect::route('works.index');
     }
 }
